d69a6e9bcb2a086171a8ae2b9fbdf43380fe272c
lxra <lxra@DESKTOP-SEURAE5>
2018-11-30 11:35:46 +1100

    Version 5.0. this release includes:
    - Fix merge conflict with file changes

--- HazMateMaven-web/src/main/java/managedBeans/hazardView_MB.java
+++ HazMateMaven-web/src/main/java/managedBeans/hazardView_MB.java
@@ -1,8 +1,10 @@
 package managedBeans;
 
 import customObjects.*;
+import ejb.DbFilesFacadeLocal;
 import entities.*;
 import ejb.DbHazardFacadeLocal;
+import ejb.DbHazardFilesFacadeLocal;
 import ejb.DbLocationFacadeLocal;
 import ejb.DbOwnersFacadeLocal;
 import ejb.DbProjectFacadeLocal;
@@ -22,6 +24,7 @@ import ejb.DbtreeLevel1FacadeLocal;
 import ejb.DbwfHeaderFacadeLocal;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.OutputStream;
 import java.io.Serializable;
 import java.math.BigInteger;
 import java.text.DecimalFormat;
@@ -63,6 +66,10 @@ import org.primefaces.model.TreeNode;
 @ViewScoped
 public class hazardView_MB implements Serializable {
 
+    @EJB
+    private DbFilesFacadeLocal dbFilesFacade;
+    @EJB
+    private DbHazardFilesFacadeLocal dbHazardFilesFacade;
     @EJB
     private DbhazardSystemStatusFacadeLocal dbhazardSystemStatusFacade;
     @EJB
@@ -157,6 +164,7 @@ public class hazardView_MB implements Serializable {
     private String deletionComment;
     private String deletionReason;
     private List<defaultViewSrchObject> checkedHazards;
+    private List<fileHeaderObject> fileHeaders;
 
     public hazardView_MB() {
     }
@@ -637,6 +645,14 @@ public class hazardView_MB implements Serializable {
         this.checkedHazards = checkedHazards;
     }
 
+    public List<fileHeaderObject> getFileHeaders() {
+        return fileHeaders;
+    }
+
+    public void setFileHeaders(List<fileHeaderObject> fileHeaders) {
+        this.fileHeaders = fileHeaders;
+    }
+
     // This method creates the search object, based on the selected parameters.
     public void constructSearchObject() {
         // Initialising a couple of variables
@@ -755,6 +771,7 @@ public class hazardView_MB implements Serializable {
         if (!listSearchedHazards.isEmpty()) {
             RequestContext.getCurrentInstance().execute("PF('widget_hazardsForm_fieldset').toggle()");
         }
+        checkedHazards = new ArrayList<>();
     }
 
     public String showExtended() {
@@ -1056,6 +1073,7 @@ public class hazardView_MB implements Serializable {
         detailCauses = dbHazardFacade.getHazardCause(detailHazard.getHazardId());
         detailConsequences = dbHazardFacade.getHazardConsequence(detailHazard.getHazardId());
         detailControls = dbHazardFacade.getControlHazard(detailHazard.getHazardId());
+        fileHeaders = dbHazardFilesFacade.findHeadersForHazard(hazardId);
     }
 
     public void clearField(String id) {
@@ -1912,5 +1930,44 @@ public class hazardView_MB implements Serializable {
             final exportedExsControl other = (exportedProControl) obj;
             return this.controlId == other.controlId;
         }
+
+    public List<String> getIdsForExport() {
+        return getCheckedHazards().stream().map(h -> h.getHazardObj().getHazardId()).collect(Collectors.toList());
     }
+    
+    public String parseSize(int fileSize) {
+        // Return a string for readability of the size field in tables
+        int order = 0;
+        String[] suffix = new String[3];
+        suffix[0] = "B";
+        suffix[1] = "kB";
+        suffix[2] = "MB";
+        double formatSize = fileSize;
+        while (formatSize / 1000 > 1) {
+            formatSize = formatSize / 1000;
+            order++;
+        }
+        DecimalFormat df = new DecimalFormat("#.###");
+        return Double.valueOf(df.format(formatSize)).toString() + " " + suffix[order];
+    }
+    
+    public void handleDownload(fileHeaderObject file) {
+        FacesContext fc = FacesContext.getCurrentInstance();
+        ExternalContext ec = fc.getExternalContext();
+        
+        ec.responseReset();
+        ec.setResponseContentType(ec.getMimeType(file.getFileName() + "." + file.getFileExtension()));
+        ec.setResponseContentLength(file.getFileSize());
+        ec.setResponseHeader("Content-Disposition", "attachment; filename=\"" + file.getFileName() + "." + file.getFileExtension() + "\"");
+        
+        try {
+            byte[] fileBlob = dbFilesFacade.findFileFromId(file.getFileId()).get(0).getFileBlob();
+            OutputStream os = ec.getResponseOutputStream();
+            os.write(fileBlob);
+        } catch (IOException e) {
+            fc.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Error:", e.getMessage()));
+        }
+        fc.responseComplete();
+    }  
+  }
 }
