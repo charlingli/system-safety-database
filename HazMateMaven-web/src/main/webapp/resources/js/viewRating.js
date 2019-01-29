ff01ad196024a970c432a23f3453ba4486901911
charlingli <charling.li@hotmail.com>
2019-01-29 11:13:46 +1100

    Version 5.4: 1. Fixed hazard ratings being jittery

--- HazMateMaven-web/src/main/webapp/resources/js/viewRating.js
+++ HazMateMaven-web/src/main/webapp/resources/js/viewRating.js
@@ -1,3 +1,7 @@
-function closeRating() {
-    PF('ratingOverlay').hide();
-}
+window.onclick = function(ev){
+    console.log(ev.target.nodeName);
+    console.log(ev.target.className);
+    if( ev.target.nodeName !== 'A' ){
+        PF('ratingOverlay').hide();
+    }
+};
